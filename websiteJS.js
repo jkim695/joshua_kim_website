bc327a5b6f03368f23414d60641c10578f78131e
jkim236 <jkim236@uw.edu>
2023-04-17 20:15:00 -0700

    first commit!

---
+++ websiteJS.js
@@ -0,0 +1,36 @@
+function scrollToTarget() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+function scrollToTarget2() {
+    var target = document.getElementById("scrollToMe");
+    target.scrollIntoView({ behavior: 'smooth' });
+}
+function scrollToTarget3() {
+    var target = document.getElementById("projects");
+    target.scrollIntoView({ behavior: 'smooth' });
+}
+const elementToToggle = document.querySelector('.scroller');
+const body = document.querySelector('body');
+
+function updateElementVisibility() {
+    const scrollTop = window.scrollY || document.documentElement.scrollTop;
+    const rect = document.getElementById('name').getBoundingClientRect();
+    const hideAt = rect.top + window.scrollY - 40;
+    const rect2 = document.getElementById('scrollToMe').getBoundingClientRect();
+    const showAt = rect2.top + window.scrollY - 55;
+    const backgroundAt = rect2.top + window.scrollY - 55; 
+
+    if (scrollTop > hideAt && scrollTop < showAt) {
+        elementToToggle.classList.add('fade-out');
+    } else {
+        elementToToggle.classList.remove('fade-out');
+    }
+
+    if (scrollTop > backgroundAt) {
+        elementToToggle.style.backgroundColor = '#1f1f1f'; // Background color when scrolled past 800px
+    } else {
+        elementToToggle.style.backgroundColor = ''; // Reset background color to default
+    }
+}
+
+window.addEventListener('scroll', updateElementVisibility);
